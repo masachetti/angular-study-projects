a1facad149a4dfffe22cc471eda394f139bb7351
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-29 16:57:08 -0400

    Create initial components

--- user-list/src/app/app.module.ts
+++ user-list/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UserListRowComponent } from './components/user-list-row/user-list-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailsComponent,
+    UserListRowComponent
   ],
   imports: [
     BrowserModule
